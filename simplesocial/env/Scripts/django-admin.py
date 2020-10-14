15737c201d49cc7a9c0aae2ef4da32b53637c88c
mustafa <mustafaadil539@gmail.com>
2020-10-14 15:17:04 +0500

    first-commit

---
+++ simplesocial/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hp\desktop\social_clone_project\simple_social_clone\simplesocial\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
